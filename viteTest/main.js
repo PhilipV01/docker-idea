a945809772de84c409f296a3be3f3d720ff34c67
r_clementz <rie.h620@gmail.com>
2022-06-02 16:04:14 +0200

    created test vite

---
+++ viteTest/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Developers!</h1>
+  <h3>This is the sample page before you guys get started<h3>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
