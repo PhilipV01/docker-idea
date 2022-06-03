311de3889ab305ddd2af2bafccdcfdb4719f2692
Norhaan-a <norhaan.ahmad1@gmail.com>
2022-06-03 15:55:34 +0200

    changed index.js

--- backend/index.js
+++ backend/index.js
@@ -2,5 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+const fs = require('fs');
+
+let indexContent = fs.readFileSync(path.join(__dirname, '../dist', 'index.html'), 'utf-8')
+  .replace(/"\//g, '"');
+
+app.get('/', (req, res) => {
+  res.send(indexContent);
+});
+
+
 app.use(express.static(path.join(__dirname, '../dist')));
 app.listen(port, () => console.log('Listening on http://localhost:' + port));
