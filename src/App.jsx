381b65231f9f0a712287c978c9af25bbc336f529
r_clementz <rie.h620@gmail.com>
2022-06-06 16:46:05 +0200

    deleted not relevant files

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ export default function App() {
   setTimeout(() => window.start((chosenCountry, chosenContinent) => {
     let arg = { service: 'worldmap', event: 'click', chosenCountry, chosenContinent };
     try {
-      parent.serviceEvents(arg);
+      // parent.serviceEvents(arg);
+      // To make it work between different domains in iframes
+      parent.postMessage(arg, '*');
     }
     catch (e) {
-      console.error('Failed calling parent.serviceEvents with arg:\n', arg);
+      console.error('Failed calling parent with arg:\n', arg);
     }
   }, 1));
 
