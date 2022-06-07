d168b6525172317770fb9b41bebc09e89457d019
shopnobaz <vision2025bd@yahoo.com>
2022-06-07 20:46:23 +0200

    deleted '/' because 'world-map', 'country-info','capital-info' are in the same folder

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ window.serviceEvents = function (action) {
 function showCountryAndCapitalInfo(action) {
   console.log(action);
   let country = encodeURIComponent(action.chosenCountry);
-  frames.countryInfo.location.href = '/country-info?country=' + country;
-  frames.capitalInfo.location.href = '/capital-info?country=' + country;
+  frames.countryInfo.location.href = 'country-info?country=' + country;
+  frames.capitalInfo.location.href = 'capital-info?country=' + country;
 
   /*Testing purposes 
     frames.countryInfo.location.href = 'http://localhost:4001/?country=' + country;
