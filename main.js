97c47bd456323836d5941445002e2a370db6bb59
shopnobaz <vision2025bd@yahoo.com>
2022-06-07 20:22:44 +0200

    adde world-map, country-info and capital-info

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ window.serviceEvents = function (action) {
 function showCountryAndCapitalInfo(action) {
   console.log(action);
   let country = encodeURIComponent(action.chosenCountry);
-  /*  frames.countryInfo.location.href = '/country-info?country=' + country;
-   frames.capitalInfo.location.href = '/capital-info?country=' + country; */
+  frames.countryInfo.location.href = '/country-info?country=' + country;
+  frames.capitalInfo.location.href = '/capital-info?country=' + country;
 
-  /* Testing purposes */
-  frames.countryInfo.location.href = 'http://localhost:4001/?country=' + country;
-  frames.capitalInfo.location.href = 'http://localhost:4003/?country=' + country;
+  /*Testing purposes 
+    frames.countryInfo.location.href = 'http://localhost:4001/?country=' + country;
+    frames.capitalInfo.location.href = 'http://localhost:4003/?country=' + country; */
 }
