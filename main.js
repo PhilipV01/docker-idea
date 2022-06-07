7ad5e0b13ae34a7094a89736dca5d4c5e9619e4b
r_clementz <rie.h620@gmail.com>
2022-06-07 22:27:46 +0200

    href for capitalInfo changed

--- main.js
+++ main.js
@@ -7,11 +7,5 @@ function showCountryAndCapitalInfo(action) {
   let country = encodeURIComponent(action.chosenCountry);
 
   frames.countryInfo.location.href = '/country-info?country=' + country;
-  frames.capitalInfo.location.href = '/country-city-info?country=' + country;
+  frames.capitalInfo.location.href = '/capital-info?country=' + country;
 }
-
-  /*Testing purposes 
-    frames.countryInfo.location.href = 'http://localhost:4001/?country=' + country;
-    frames.capitalInfo.location.href = 'http://localhost:4003/?country=' + country; */
-
-
