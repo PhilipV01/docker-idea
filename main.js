fb169fa73b6c501a7ed8e640218c8f1b7c42809b
shopnobaz <vision2025bd@yahoo.com>
2022-06-08 19:18:02 +0200

    paths

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ function showCountryAndCapitalInfo(action) {
   console.log(action);
   let country = encodeURIComponent(action.chosenCountry);
 
-  frames.countryInfo.location.href = '/country-info?country=' + country;
-  frames.capitalInfo.location.href = '/capital-info?country=' + country;
+  frames.countryInfo.location.href = 'country-info?country=' + country;
+  frames.capitalInfo.location.href = 'capital-info?country=' + country;
 }
