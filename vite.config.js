805581dfb4f4e78fcf1538fe364b9469b66eab5e
shopnobaz <vision2025bd@yahoo.com>
2022-06-10 10:16:03 +0200

    changed vite config localhost 4003 as capital-info-main

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     hmr: false,
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'http://localhost:4003',
         changeOrigin: true,
         secure: false
       }
